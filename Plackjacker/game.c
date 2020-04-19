65e5241389ea9beadea72f65f7876e08a0516aa9
millstar <56350571+millstar@users.noreply.github.com>
2020-04-20 01:00:05 +0700

    Update Code
    -Add function play again
    -Draw a card
    -Win/Lose

--- Plackjacker/game.c
+++ Plackjacker/game.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <process.h>  
+#include <windows.h>
+
+HANDLE wHnd;    // Handle to write to the console.
+HANDLE rHnd;    // Handle to read from the console.
 
 //Global Variabales
 int i;
@@ -33,13 +37,47 @@ struct Deck
 void menu();
 void tutorial();
 void game();
-void printplayer(struct hand player);
+void newgame();
+void printplayer(struct hand player, int amount);
 int checkdeck(char card[], struct Deck deck2, int count);
 int givescore(char card);
+int hitstay();
+int checkace(char card);
 struct Deck shuffle();
+struct hand sort(struct hand player, int amount);
+
+//Check win functons
+int pair(struct hand player);
+int twopair(struct hand player);
+int twentyone(struct hand player);
+int pair21(struct hand player);
+int twopair21(struct hand player);
+int threeofkind(struct hand player);
+int threeofkind21(struct hand player);
+int fivecards(struct hand player);
+int straight(struct hand player);
+int flush(struct hand player);
+int fullhouse(struct hand player);
+int fourofkind(struct hand player);
+int straightflush(struct hand player);
+int blackjack(struct hand player);
+
 
 int main()
 {
+	// Set up the handles for reading/writing:
+    wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+    rHnd = GetStdHandle(STD_INPUT_HANDLE);
+    // Change the window title:
+    SetConsoleTitle("Advanced C tutorial - programming-technique.blogspot.com");
+    // Set up the required window size:
+    SMALL_RECT windowSize = {0, 0, 80, 30};
+    SetConsoleWindowInfo(wHnd, 1, &windowSize);
+    // Change the console window size:
+        // Create a COORD to hold the buffer size:
+    COORD bufferSize = {10, 10};
+    SetConsoleScreenBufferSize(wHnd, bufferSize);
+
 	menu();
 	return 0;
 }
@@ -73,17 +111,20 @@ void game()
 	system("cls");
 
 	//Declare variables
-	int turn = 0;
+	int j = 0;
+	int turn = 2;
 	int p_score = 0;
 	int d_score = 0;
-	int ace_p = 0;
-	int ace_d = 0;
+	int p_ace = 0;
+	int d_ace = 0;
+	int p_win = 0;
+	int d_win = 0;
 
 	struct hand player;
 	struct hand dealer;
 	struct Deck outdeck;
 
-	printf("\n\n================================================================================\n");
+	printf("================================================================================\n");
 	printf("                                   Game Started!!!\n");
 	printf("================================================================================\n\n");
 
@@ -92,10 +133,6 @@ void game()
 
 	outdeck = shuffle();
 
-	//Print Deck
-	for (i = 0;i < 52;i++) {
-		printf("%c%c\n", outdeck.card[i].number, outdeck.card[i].symbol);
-	}
 
 	// Reset Hand
 	for (i = 0;i < 5;i++) {
@@ -105,35 +142,117 @@ void game()
 		dealer.card[i].symbol = '0';
 	}
 
-	//Player hand
-	printf("Your hand have: ");
-	printplayer(player);
+	//First two card
+	for (i = 0;i < 2;i++) {
+
+		player.card[j].number = outdeck.card[i].number;
+		player.card[j].symbol = outdeck.card[i].symbol;
+		p_score += givescore(player.card[j].number);
+		p_ace += checkace(player.card[j].number);
+
+		if (i == 1) {
+			dealer.card[j].number = 'X';
+			dealer.card[j].symbol = 'X';
+		} else {
+			dealer.card[j].number = outdeck.card[i+1].number;
+			dealer.card[j].symbol = outdeck.card[i+1].symbol;
+			d_score += givescore(dealer.card[j].number);
+			d_ace += checkace(dealer.card[j].number);
+		}
+
+		j++;//Count Card
+	}
+
+
+	//Player's hand
+	player = sort(player, j);
+	printf("Your hand have:");
+	printplayer(player, j);
 	printf("\n");
 	printf("Your Score: %d\n\n", p_score);
 
-	printf("Dealer's hand has: ");
-	printplayer(dealer);
+
+	//Dealer's hand
+	printf("Dealer's hand has:");
+	printplayer(dealer, j);
 	printf("\n");
-	printf("Dealer Score: %d\n", d_score);
+	printf("Dealer Score: %d\n\n", d_score);
+
+	//Player turn
+	//When player want to hit a card.
+	while (hitstay()) 
+		{
+			turn++;
+
+			if (turn == 6)
+				break;
+
+			player.card[j].number = outdeck.card[j].number;
+			player.card[j].symbol = outdeck.card[j].symbol;
+			p_score += givescore(outdeck.card[j].number);
+			p_ace += checkace(player.card[j].number);
+
+			j++;//Count card
+
+			//Show hand
+			player = sort(player, j);
+			printf("\nYour hand have:");
+			printplayer(player, j);
+			printf("\n");
+			printf("Your Score: %d\n\n", p_score);
+
+			if (p_score >= 21)
+				break;
+
+			scanf("%c", &choice);
+
+		}
+
+	//Check the player's hand
+
+
+	printf("%d\n\n", p_win);
+
+	printf("P: %d D: %d\n\n", p_ace, d_ace);
+	
+	newgame();
 }
 
 void tutorial()
 {
 	system("cls");
-	printf("\n\n================================================================================\n");
+	printf("================================================================================\n");
 	printf("                               Tutorial and Rules\n");
 	printf("================================================================================\n\n");
 }
 
-void printplayer(struct hand player)
+void newgame()
 {
-	for (i = 0;i < 5;i++) {
+	while (1) {
+		printf("Do you want to start new game? (Y/N): ");
+		scanf(" %c", &choice);
+		printf("\n");
+
+		if (toupper(choice) == 'Y') {
+			game();
+			break;
+		}
+		else if (toupper(choice) == 'N') {
+			menu();
+			break;
+		} 
+	}
+}
+
+//Function parts
+void printplayer(struct hand player,  int amount)
+{
+	for (i = 0;i < amount;i++) {
 		if (player.card[i].number == ':')
 			printf(" 10%c", player.card[i].symbol);
 		else
 			printf(" %c%c", player.card[i].number, player.card[i].symbol);
 	}
-
 }
 
 
@@ -150,6 +269,44 @@ int checkdeck(char card[], struct Deck deck, int count)
 	}
 
 	return chk;
+}
+
+int givescore(char card)
+{
+	if (card == 'X')
+		return 0;
+	else if (card == 'J' || card == 'Q' || card == 'K')
+		return 10;
+	else if (card == 'A')
+		return 11;
+	else
+		return (int)(card) - 48;
+}
+
+int hitstay()
+{
+	while (1)
+	{
+		printf("Do you want Hit or Stay? (H/S): ");
+		scanf(" %c", &choice);
+
+		if (toupper(choice) == 'H') {
+			return 1;
+			break;
+		}
+		else if (toupper(choice) == 'S') {
+			return 0;
+			break;
+		}
+	}		
+}
+
+int checkace(char card)
+{
+	if (card == 'A')
+		return 1;
+	else
+		return 0;
 }
 
 struct Deck shuffle()
@@ -185,3 +342,56 @@ struct Deck shuffle()
 
 	return deck;
 }
+
+struct hand sort(struct hand player, int amount)
+{
+	int k;
+	char temp1, temp2;
+
+	for (i = 0;i < amount;i++) 
+	{
+		for(k = i + 1;k < amount;k++)
+		{
+			if (player.card[i].number > player.card[k].number) {
+				if (player.card[i].number == 'A') {
+					continue;
+				}
+				else if (player.card[i].number == 'K') {
+					temp1 = player.card[i].number;
+					temp2 = player.card[i].symbol;
+					player.card[i].number = player.card[k].number;
+					player.card[i].symbol = player.card[k].symbol;
+					player.card[k].number = temp1;
+					player.card[k].symbol = temp2;
+				}
+				else {
+					temp1 = player.card[i].number;
+					temp2 = player.card[i].symbol;
+					player.card[i].number = player.card[k].number;
+					player.card[i].symbol = player.card[k].symbol;
+					player.card[k].number = temp1;
+					player.card[k].symbol = temp2;
+				}
+			}
+		}
+	}
+
+	return player;
+}
+
+
+//int pair(struct hand player)
+
+//int twopair(struct hand player);
+//int twentyone(struct hand player);
+//int pair21(struct hand player);
+//int twopair21(struct hand player);
+//int threeofkind(struct hand player);
+//int threeofkind21(struct hand player);
+//int fivecards(struct hand player);
+//int straight(struct hand player);
+//int flush(struct hand player);
+//int fullhouse(struct hand player);
+//int fourofkind(struct hand player);
+//int straightflush(struct hand player);
+//int blackjack(struct hand player);
